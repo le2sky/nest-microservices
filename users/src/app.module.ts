9aff3f3c5c485bb463dd8fc4fe434385048f7f4c
leesky <leehaneul0623@gmail.com>
2022-01-30 15:43:38 +0900

    [#8] setting the users microservices database

---
+++ users/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'users_db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'users',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
